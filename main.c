e2d8435ba9033ce0de566ed26078f9b8bca443bd
mwirigi <bmmaingi17@gmail.com>
2022-09-15 11:08:37 +0300

    live learning session: c-Hello WOrld

---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+ * main - entrypoint
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	int squared;
+	squared = getSquare(2);
+	printf("the value of squared is: %d\n", squared);
+	return(0);
+}
